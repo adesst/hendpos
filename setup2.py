7e96eacb068412f189dddefc95957ed3d4e90f24
adesanto asman <adesantoasman@gmail.com>
2013-08-01 12:38:13 +0700

    reorder the requires

--- setup2.py
+++ setup2.py
@@ -8,17 +8,18 @@ setup(
     version = '1.0',
     install_requires = [
         'Mysql-python==1.2.3',
-        'Flask==0.9',
-        'pytz',
         'Flask-Babel==0.8',
         'Flask-sqlalchemy==0.16',
+        'Flask-WTF==0.8.2',
+        'FormAlchemy',
         'sqlalchemy==0.8',
         'Flask-WTF==0.8.2',
         'WTForms==1.0.3',
-        'FormAlchemy',
+        'Flask==0.9',
+        'gevent-websocket',
         'greenlet',
         'gevent',
-        'gevent-websocket',
+        'pytz',
     ],
 )
 
