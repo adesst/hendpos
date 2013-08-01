5fb0580b09b9946b7bc59d071690037975c78979
adesanto asman <adesantoasman@gmail.com>
2013-08-01 11:47:23 +0700

    set WTF to 0.8.2

--- setup2.py
+++ setup2.py
@@ -5,13 +5,14 @@ from setuptools.command.install import install
 
 setup(
     name = 'hendpos',
+    version = '1.0',
     install_requires = [
         'Mysql-python',
         'Flask==0.9',
         'pytz',
         'Flask-Babel',
         'Flask-sqlalchemy',
-        'Flask-WTF',
+        'Flask-WTF==0.8.2',
         'FormAlchemy',
         'greenlet',
         'gevent',
