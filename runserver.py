ea2fb0b81a9fc65f50fa14cc99333388fd71d99a
adesanto asman <adesantoasman@gmail.com>
2013-08-02 18:15:58 +0700

    adding verbose info

--- runserver.py
+++ runserver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+import os
 import logging
 import traceback
 from gevent.pywsgi import WSGIServer
@@ -32,6 +33,9 @@ class XWSGIServer(WSGIServer):
             logging.error('%s ' % ( traceback.print_exc))
 
 if __name__ == '__main__':
+    if os.name == 'nt':
+        print 'To Close this application just close this window.'
+
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(msg)s", filename="log.txt")
 
     class SimpleLog():
@@ -41,7 +45,9 @@ if __name__ == '__main__':
             logging.info(msg.rsplit("\n"))
 
     try:
+        print 'Initializing...'
         http_server = XWSGIServer(('',5000), my_app, handler_class=WebSocketHandler, log=SimpleLog())
+        print 'Show time :D'
         http_server.serve_forever()
     except Exception as e:
         print e
